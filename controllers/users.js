146c5304d3379a5b3e94ade63d88ebd17fa7406b
dmitrybdrv <incubator-it@protonmail.com>
2024-03-07 11:46:41 +0300

    login flow pre complete. added new deps: bcrypt, jsonwebtoken. added new plugins: .env. start models structure completed.

--- controllers/users.js
+++ controllers/users.js
@@ -1,5 +1,44 @@
+const prisma = require('../prisma/prisma-client')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+
+/**
+ *
+ * @route POST /apt/user/login
+ * @desc Логин
+ * @access Public
+ */
 const login = async (req, res) => {
-        res.send('login');
+    const {email, password} = req.body
+
+    //условие - на отсутствие введёного email или password
+    if (!email || !password) {
+        return res.status(400).json({message: 'Пожалуйста, заполните обязательные поля'});
+    }
+
+    //поиск пользователя в базе.
+    const user = await prisma.user.findFirst({
+        where: {
+            email,
+        },
+    });
+
+    //Верный пароль - если пользователь нашёлся в базе и введённый пароль соответствует паролю в базе
+    const isPasswordCorrect = user && (await bcrypt.compare(password, user.password));
+
+    //условие - на соответствие логина и пароля найденного в базе пользователя, введённым данным для авторизации
+    if (isPasswordCorrect && user) {
+        return res.status(200).json({
+            id: user.id,
+            email: user.email,
+            name: user.name
+        })
+    } else {
+        return res.status(400).json({
+            message: 'Не верно введён логин или пароль'
+        })
+    }
+
 }
 
 const register = async (req, res) => {
