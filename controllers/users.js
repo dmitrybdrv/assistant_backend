c30dac47bd8f524e18e249533f5c0ef59510c128
dmitrybdrv <incubator-it@protonmail.com>
2024-03-07 14:41:53 +0300

    register API request completed.

--- controllers/users.js
+++ controllers/users.js
@@ -9,15 +9,20 @@ const jwt = require('jsonwebtoken')
  * @access Public
  */
 const login = async (req, res) => {
-    const {email, password} = req.body
+    const {email, password, rememberMe} = req.body
 
-    //условие - на отсутствие введёного email или password
+    //условие - на отсутствие введёного email или password при авторизации
     if (!email || !password) {
         return res.status(400).json({message: 'Пожалуйста, заполните обязательные поля'});
     }
 
+    //условие - поверка валидности пароля (длина)
+    if (password.length <= 5 || password.length > 30) {
+        return res.status(400).json({message: 'Пароль должен состоять от 6 до 30 символов'});
+    }
+
     //поиск пользователя в базе.
-    const user = await prisma.user.findFirst({
+    const user = await prisma.prisma.user.findFirst({
         where: {
             email,
         },
@@ -26,12 +31,17 @@ const login = async (req, res) => {
     //Верный пароль - если пользователь нашёлся в базе и введённый пароль соответствует паролю в базе
     const isPasswordCorrect = user && (await bcrypt.compare(password, user.password));
 
-    //условие - на соответствие логина и пароля найденного в базе пользователя, введённым данным для авторизации
-    if (isPasswordCorrect && user) {
+    //создание jwt токена
+    const secret = process.env.JWT_SECRET
+
+    //условие - на соответствие логина и пароля (+наличие secret записи в .env) найденного в базе пользователя, введённым данным для авторизации
+    if (isPasswordCorrect && user && secret) {
+        //при соответствии - положительный ответ (объект с данными)
         return res.status(200).json({
             id: user.id,
             email: user.email,
-            name: user.name
+            name: user.name,
+            token: jwt.sign({id: user.id}, secret, {expiresIn: '1d'})
         })
     } else {
         return res.status(400).json({
@@ -41,8 +51,71 @@ const login = async (req, res) => {
 
 }
 
+/**
+ *
+ * @route POST /api/user/register
+ * @desc Регистрация
+ * @access Public
+ */
 const register = async (req, res) => {
-    res.send('register');
+    const {email, password, name} = req.body
+
+    //условие - на отсутствие введёного email или password при регистрации
+    if (!email || !password || !name) {
+        return res.status(400).json({message: 'Пожалуйста, заполните обязательные поля'});
+    }
+
+    //поиск уже существующего в базе пользователя
+    const alreadyRegisteredUser = await prisma.prisma.user.findFirst({
+        where: {
+            email,
+        }
+    })
+
+    //условие - поверка валидности пароля (длина)
+    if (password.length <= 5 || password.length > 30) {
+        return res.status(400).json({message: 'Пароль должен состоять от 6 до 30 символов'});
+    }
+
+    //условие - если уже существует в базе
+    if (alreadyRegisteredUser) {
+        return res.status(400).json({
+            message: 'Пользователь с таким email уже существует'
+        })
+    }
+
+    //зашифровывание пароля (для последующей записи в базу зашифрованного пароля)
+    const salt = await bcrypt.genSalt(10)
+    const hashedPassword = await bcrypt.hash(password, salt)
+
+    //создание нового пользователя (если не найден в базе)
+    const newUser = await prisma.prisma.user.create({
+        data: {
+            name,
+            email,
+            password: hashedPassword,
+        }
+    })
+
+    //создание jwt токена
+    const secret = process.env.JWT_SECRET
+
+    //условие - если пользователь создан и secret в наличии, возвращается объект с id, email, token
+    if (newUser && secret) {
+        res.status(201).json({
+            id: newUser.id,
+            name: newUser.name,
+            email: newUser.email,
+            //создание jwt токена, который обнулиться через 1 день.
+            //TODO изменить период протухания токена на меньшее значение
+            token: jwt.sign({id: newUser.id}, secret, {expiresIn: '1d'})
+        })
+    } else {
+        return res.status(400).json({
+            message: 'Не удалось создать пользователя'
+        })
+    }
+
 }
 
 const current = async (req, res) => {
